c4fc2ff6e88e7af701a01922d029c943cac61210
DiazLuisAlejandro <alejandrodiazluisv@gmail.com>
2025-11-19 17:14:38 +0000

    Clase 19

---
+++ Unidad3/crud-mongo/src/main/java/com/docencia/personas/repository/PersonaRepository.java
@@ -0,0 +1,20 @@
+package com.docencia.personas.repository;
+
+import com.docencia.personas.model.*;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonaRepository extends MongoRepository<Persona, String> {
+
+    Optional<Persona> findByEmail(String email);
+
+    List<Persona> findByNombreContainingIgnoreCase(String nombre);
+
+    List<Persona> findByEdadBetween(Integer min, Integer max);
+
+    @Query("{ 'direccion.ciudad': ?0 }")
+    List<Persona> findByCiudad(String ciudad);
+}
---
+++ Unidad3/crud-mongo/src/main/java/com/docencia/personas/services/implementation/PersonaService.java
@@ -0,0 +1,42 @@
+package com.docencia.personas.services.implementation;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.docencia.personas.model.Persona;
+import com.docencia.personas.repository.PersonaRepository;
+import com.docencia.personas.services.IPersonaService;
+
+@Service
+public class PersonaService implements IPersonaService {
+
+    private final PersonaRepository personaRepository;
+
+    public PersonaService(PersonaRepository personaRepository){
+        this.personaRepository=personaRepository;
+    }
+
+    @Override
+    public List<Persona> findAll() {
+        return personaRepository.findAll();
+    }
+
+    @Override
+    public Persona findById(String id) {
+        return null;
+        //return personaRepository.findById(id);
+    }
+
+    @Override
+    public Persona save(Persona persona) {
+        return personaRepository.save(persona);
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        personaRepository.deleteById(id);
+        return true;
+    }
+
+}
